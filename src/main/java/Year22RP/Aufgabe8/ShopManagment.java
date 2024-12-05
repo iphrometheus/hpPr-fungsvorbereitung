a11ad0defa76cfc77fcec566a87d8498b35ff357
linus <linus>
2024-12-05 15:21:32 +0100

    Aufgabe8

--- src/main/java/Year22RP/Aufgabe8/ShopManagment.java
+++ src/main/java/Year22RP/Aufgabe8/ShopManagment.java
@@ -8,10 +8,12 @@ public class ShopManagment {
     public boolean addPart(Part part, int amount){
         if(part == null || amount <= 0) return false;
 
-        Integer tmp = stock.get(part) != null ? stock.get(part) + amount : amount;
-        if(tmp == Integer.MIN_VALUE) return false;
+        Integer tmp = stock.get(part);
+        if(tmp > Integer.MAX_VALUE - amount){ /// prüft ob tmp größer ist als die max anzahl die noch addiert werden kann
+            return false;
+        }
         
-        stock.put(part, tmp);
+        stock.put(part, tmp + amount);
         return true;
     }
 
@@ -20,7 +22,7 @@ public class ShopManagment {
 
         Integer tmp = stock.get(part);
         if(tmp == null) return false;
-        int x = amount - tmp;
+        int x = tmp - amount;
         if(x < 0){
             return false;
         } if (x == 0) {
