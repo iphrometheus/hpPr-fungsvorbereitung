70bdab6f229c608193db289f2e54bce6c940e4f2
linus <linus>
2024-12-05 13:31:36 +0100

    AufgabeD

---
+++ src/main/java/Year22RP/Aufgabe7/QueueMainD.java
@@ -0,0 +1,17 @@
+package Year22RP.Aufgabe7;
+
+public class QueueMainD {
+    public static void main(String[] args) {
+        Queue<String> q = new Queue<>();
+            q.enqueue("AA");
+            q.enqueue("AC");
+            q.enqueue("EE");
+            q.enqueue("ZZ");
+            q.enqueue("AZ");
+
+        Queue<String> q2 = q.filter(e -> e.startsWith("A"));
+        for (String s : q2) {
+            System.out.println(s);
+        }
+    }
+}
