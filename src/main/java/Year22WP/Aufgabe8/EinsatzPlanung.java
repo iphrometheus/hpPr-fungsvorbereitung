23225e6c2ddc7ed4852e6fdb170e289d5a811104
linus <linus>
2024-12-05 15:33:47 +0100

    Year22WPAufgabe8 update

--- src/main/java/Year22WP/Aufgabe8/EinsatzPlanung.java
+++ src/main/java/Year22WP/Aufgabe8/EinsatzPlanung.java
@@ -2,33 +2,27 @@ package Year22WP.Aufgabe8;
 
 import java.util.HashMap;
 
-public class EinsatzPlanung{
-
+public class EinsatzPlanung {
     private HashMap<Rentier, Integer> einsatz = new HashMap<>();
 
     public boolean plusVerwendung(Rentier rentier, int minuten){
-        if(rentier == null || minuten < 0) return false;
-        if(!einsatz.containsKey(rentier)) einsatz.put(rentier, minuten);
-        int update = einsatz.remove(rentier);
-        try{
-            minuten += update;
-        } catch(Exception e){
-            einsatz.put(rentier, update);
+        if(rentier == null || minuten <= 0){
             return false;
         }
-        einsatz.put(rentier, minuten);
-        return true;
-    }
-    public boolean minusVerwendung(Rentier rentier, int minuten){
-        if(rentier == null || minuten < 0 || !einsatz.containsKey(rentier)) return false;
-        int update = einsatz.get(rentier);
-        int newupdate = minuten - update;
-        if(newupdate <= 0){
-            einsatz.put(rentier, update);
-            return false;
-        }
-        einsatz.put(rentier, newupdate);
+        int got = einsatz.getOrDefault(rentier, 0);
+
+        if(got > Integer.MAX_VALUE - minuten) return false;
+        einsatz.put(rentier, got+minuten);
         return true;
     }
 
+    public boolean minusVerwendung(Rentier rentier, int minuten){
+        if(rentier == null || minuten <= 0){
+            return false;
+        }
+        Integer got = einsatz.get(rentier);
+        if(got == null) return false;
+        einsatz.put(rentier, got - minuten);
+        return true;
+    }
 }
