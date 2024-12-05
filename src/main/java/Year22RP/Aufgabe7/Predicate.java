a53940b44f6d6867cc988d3ba7b6bc74d22d2f78
linus <linus>
2024-12-05 13:29:56 +0100

    Aufgabe 7A

---
+++ src/main/java/Year22RP/Aufgabe7/Predicate.java
@@ -0,0 +1,6 @@
+package Year22RP.Aufgabe7;
+
+@FunctionalInterface
+public interface Predicate<E> {
+    boolean test(E o);
+}
---
+++ src/main/java/Year22RP/Aufgabe7/Queue.java
@@ -0,0 +1,98 @@
+package Year22RP.Aufgabe7;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Queue<E> implements Iterable<E>{
+    
+    /// Aufgabe A:
+    private SSL<E> head;
+    private SSL<E> tail;
+
+    private static class SSL<E>{
+        public E element;
+        public SSL<E> next;
+
+        SSL(E element, SSL<E> next){
+            this.element = element;
+            this.next = next;
+        }
+    }
+
+    boolean isEmpty(){
+        return head == null;
+    }
+
+    void enqueue(E ele){
+        if(ele == null) return;
+        if(head == null){
+            head = tail = new SSL<>(ele, null);
+        } else{
+            tail.next = new SSL<>(ele, null);
+            tail = tail.next;
+        }
+    }
+
+    E getFirst(){
+        return head.element;
+    }
+
+    E dequeue(){
+        E tmp = head.element;
+        head = head.next;
+        return tmp;
+    }
+
+
+    /// Aufgabe B:
+    public Iterator<E> iterator(){
+        return new QIter();
+    }
+
+
+    private class QIter implements Iterator<E>{
+        private SSL<E> iter = head;
+
+        @Override
+        public boolean hasNext(){   /// darf nicht == sein, sondern muss != sein.
+            return iter != null;
+        }
+
+        @Override
+        public E next(){
+            if(iter == null) throw new NoSuchElementException();
+            E tmp = iter.element;
+            iter = iter.next;
+            return tmp;
+        }
+    }
+
+    /// Aufgabe C:
+    /// Damit min mit Queue.min() aufgerufen werden kann, muss min static sein
+    ///
+    static <Z extends Comparable<Z>> Z min(Queue<Z> queue){
+        if(queue == null || queue.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        Z tmp = queue.head.element;
+        for(Z i : queue){
+            if(i.compareTo(tmp) < 0){
+                tmp = i;
+            }
+        }
+        return tmp;
+    }
+
+    /// Aufgabe D:
+
+    Queue<E> filter(Predicate<E> predicate){
+        Queue<E> tmp = new Queue<>();
+        for(E i: this){
+            if(predicate.test(i)){
+                tmp.enqueue(i);
+            }
+        }
+        return tmp;
+    }
+
+}
